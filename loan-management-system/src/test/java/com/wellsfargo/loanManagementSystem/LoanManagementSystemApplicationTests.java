0e52d8e3005617e1a7d7ce8d4d387f2130e4b497
gaurav597 <cgaurav1682@gmail.com>
2023-09-08 11:24:38 +0530

    added spring template

---
+++ loan-management-system/src/test/java/com/wellsfargo/loanManagementSystem/LoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.loanManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
