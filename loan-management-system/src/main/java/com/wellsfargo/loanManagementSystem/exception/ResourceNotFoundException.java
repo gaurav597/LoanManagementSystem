1c608d999571b56a74c0d1fb4364eac577c8f419
Gaurav Singh Choudhary <80539295+gaurav597@users.noreply.github.com>
2023-09-12 23:47:07 +0530

    Registration and login commit

--- loan-management-system/src/main/java/com/wellsfargo/loanManagementSystem/exception/ResourceNotFoundException.java
+++ loan-management-system/src/main/java/com/wellsfargo/loanManagementSystem/exception/ResourceNotFoundException.java
@@ -1,2 +1,14 @@
-package com.wellsfargo.loanManagementSystem.exception;public class ResourceNotFoundException {
+package com.wellsfargo.loanManagementSystem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
