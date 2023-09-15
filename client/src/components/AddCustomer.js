194dfc5d04771cf8221564688ecabb30d8eb8e1c
Gaurav Singh Choudhary <80539295+gaurav597@users.noreply.github.com>
2023-09-15 12:01:56 +0530

    react-bootstrap

--- client/src/components/AddCustomer.js
+++ client/src/components/AddCustomer.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
 export default function AddCustomer() {
-  return (
-    <div>
-        <h1>Add Customer</h1>
-        <label>Employee Id</label>
-        <input/>  
-        <label>Designation</label>
-        <input/>
-        
-    </div>
-  )
+    return (
+
+        <div className='container'>
+            <h1>Add Customer Data</h1>
+            
+        </div>
+    )
 }
