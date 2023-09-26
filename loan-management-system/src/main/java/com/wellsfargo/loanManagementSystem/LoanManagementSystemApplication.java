e897fa781f142c96f552f07affeddf883a635fb3
Mrinal-M <mrinal.m.iiitb@gmail.com>
2023-09-26 23:04:32 +0530

    Added Test file for EmployeeController

--- loan-management-system/src/main/java/com/wellsfargo/loanManagementSystem/LoanManagementSystemApplication.java
+++ loan-management-system/src/main/java/com/wellsfargo/loanManagementSystem/LoanManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.wellsfargo.loanManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@///ComponentScan("com.wellsfargo.loanManagementSystem.service")
 public class LoanManagementSystemApplication {
 
 	public static void main(String[] args) {
