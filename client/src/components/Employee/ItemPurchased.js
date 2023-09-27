8248bb22c7c1f930d618dc25aa82df2b13d0956f
Nithyasri S <nithyasri0913@gmail.com>
2023-09-27 18:36:46 +0530

    added card for employee pages

--- client/src/components/Employee/ItemPurchased.js
+++ client/src/components/Employee/ItemPurchased.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import EmployeeDashboard from "./EmployeeDashboard";
 import Table from "react-bootstrap/Table";
 import ItemMasterService from "../../services/ItemMasterService";
-
+import "../../styles/Login.css"
 const ItemPurchased = (props) => {
 
   const [data, setData] = useState([]);
@@ -13,63 +13,61 @@ const ItemPurchased = (props) => {
     })
   }, []);
 
-
   return (
-    <div>
+    <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto" style={{ justifyContent: "center", fontFamily: "Libre Baskerville" }}>
       <br />
-      <div
-        style={{ color: "white", display: "flex", justifyContent: "center" }}
-      >
-        <h1>Purchased Items</h1>
-      </div>
-      <br />
-
-      <div className="row justify-content-center">
-
-        <div class="row d-flex text-center" style={{ color: "white", display: "flex", justifyContent: "center" }}>
-          <div class="col-lg-4" style={{ justifyContent: "center" }}>
-            <span style={{ fontSize: "16px" }}><b>Employee ID: {props['id']}</b></span>
-          </div>
-          <div class="col-lg-4">
-            <span style={{ fontSize: "16px" }}><b>Designation: {props['des']}</b></span>
-          </div>
-          <div class="col-lg-4">
-            <span style={{ fontSize: "16px" }}><b>Department: {props['dept']}</b></span>
-          </div>
-        </div>
-        <br /><br /><br />
-        <Table striped bordered hover
-          style={{ width: "50%", borderRadius: "20px", backgroundColor: "#77dd77", textAlign: "center" }}
+      <div class="card card0" style={{ borderRadius: "35px", justifyContent: "center", paddingLeft: "120px", paddingRight: "120px" }}>
+        <br /> <br />
+        <div
+          style={{ color: "black", display: "flex", justifyContent: "center" }}
         >
-          <thead class="table-success">
-            <tr>
-              <th>Issue ID</th>
-              <th>Item Description</th>
-              <th>Item Make</th>
-              <th>Item Category</th>
-              <th>Item Valuation</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item) => (
+          <br /> <br />
+          <h1>Purchased Items</h1>
+        </div>
+        <br />
+
+        <div className="row justify-content-center">
+          <div class="row d-flex text-center" style={{ color: "black", display: "flex", justifyContent: "center" }}>
+
+            <div class="row d-flex text-center" style={{ color: "black", display: "flex", justifyContent: "center" }}>
+              <div class="col-lg-4" style={{ justifyContent: "center" }}>
+                <span style={{ fontSize: "16px" }}><b>Employee ID: {props['id']}</b></span>
+              </div>
+              <div class="col-lg-4">
+                <span style={{ fontSize: "16px" }}><b>Designation: {props['des']}</b></span>
+              </div>
+              <div class="col-lg-4">
+                <span style={{ fontSize: "16px" }}><b>Department: {props['dept']}</b></span>
+              </div>
+            </div>
+          </div>
+          <br /><br /> <br />
+          <Table striped bordered hover
+            style={{ width: "70%", borderRadius: "20px", backgroundColor: "#77dd77", textAlign: "center" }}
+          >
+            <thead class="table-success">
               <tr>
-                <td>{item.issueId}</td>
-                <td>{item.itemDescription}</td>
-                <td>{item.itemMake}</td>
-                <td>{item.itemCategory}</td>
-                <td>{item.itemValuation}</td>
+                <th>Issue ID</th>
+                <th>Item Description</th>
+                <th>Item Make</th>
+                <th>Item Category</th>
+                <th>Item Valuation</th>
               </tr>
-            ))}
-            {/* {loans.map((loan) => (
-              <tr key={loan.id}>
-                <td> {loan.id} </td>
-                <td> {loan.type} </td>
-                <td> {loan.duration} </td>
-                <td> {loan.IssueDt} </td>
-              </tr>
-            ))} */}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr>
+                  <td>{item.issueId}</td>
+                  <td>{item.itemDescription}</td>
+                  <td>{item.itemMake}</td>
+                  <td>{item.itemCategory}</td>
+                  <td>{item.itemValuation}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <br /> <br /> <br />
       </div>
     </div>
   );
